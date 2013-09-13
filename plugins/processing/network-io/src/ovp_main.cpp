2f73f6aab776f8715102040efbb396ac90b38cac
Jussi Lindgren <dev@openvibe.inria.fr>
2013-09-13 18:06:01 +0200

    plugins/processing/network-io:  * Renamed the StimulusTCPWriter box to TCPWriter  * Modified the TCP Writer box to also support Signal and Streamed Matrix input streams  * Wrote brief documentation for the TCP Writer box

--- plugins/processing/network-io/src/ovp_main.cpp
+++ plugins/processing/network-io/src/ovp_main.cpp
@@ -1,15 +1,15 @@
 
 #include "ovp_defines.h"
 
-#include "box-algorithms/ovpCBoxAlgorithmStimulusTCPWriter.h"
+#include "box-algorithms/ovpCBoxAlgorithmTCPWriter.h"
 
 OVP_Declare_Begin();
+	
+	OVP_Declare_New(OpenViBEPlugins::NetworkIO::CBoxAlgorithmTCPWriterDesc);
 
-	OVP_Declare_New(OpenViBEPlugins::NetworkIO::CBoxAlgorithmStimulusTCPWriterDesc);
-
-	rPluginModuleContext.getTypeManager().registerEnumerationType(OVP_TypeID_StimulusTCPWriter_OutputStyle,"Output style");
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeID_StimulusTCPWriter_OutputStyle,"Raw",TCPWRITER_RAW);
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeID_StimulusTCPWriter_OutputStyle,"Hex",TCPWRITER_HEX);
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeID_StimulusTCPWriter_OutputStyle,"String",TCPWRITER_STRING);
+	rPluginModuleContext.getTypeManager().registerEnumerationType(OVP_TypeID_TCPWriter_OutputStyle,"Stimulus output");
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeID_TCPWriter_OutputStyle,"Raw",TCPWRITER_RAW);
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeID_TCPWriter_OutputStyle,"Hex",TCPWRITER_HEX);
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeID_TCPWriter_OutputStyle,"String",TCPWRITER_STRING);
 
 OVP_Declare_End();
