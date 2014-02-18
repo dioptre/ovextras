b5a42caafb1c54f176b7654307d9e40c889575c3
Guillermo Andrade B <guillermo.Andrade-Barroso@inria.fr>
2014-02-18 16:59:34 +0100

    Add a GUI test using sikuli tool DartTestFile.txt dectect if sikuli tool is available in path. GUI interface maybe need specific OS scripts

---
+++ applications/platform/designer/test/testStartInterface.UNIX.sikuli/testStartInterface.UNIX.py
@@ -0,0 +1,25 @@
+def setUp(self):
+    import os
+    ov_binany_path=os.environ['OV_BINARY_PATH']
+    self.terminal = App.open("xterm")
+    wait(1)
+    type(ov_binany_path +"/openvibe-designer.sh --no-session-management"+ Key.ENTER)
+    wait("StartInterface.png",20000)
+def test_createSimpleScenarioAndRun(self):
+    click(Pattern("Datagenerati.png").targetOffset(-70,-1))
+    dragDrop("Sinusoscilla-1.png",Pattern("InterfaceWithSignal.png").similar(0.16).targetOffset(-232,-180))
+    assert(exists("SinusOscillatorBoxSelected.png"))
+    click(Pattern("Visualisatio.png").targetOffset(-62,0))
+    click(Pattern("Basic.png").targetOffset(-50,-1))
+    dragDrop("ESignaldispl.png", Pattern("itENEExlJEll.png").similar(0.16).targetOffset(-288,-26))
+    assert(exists("SignalDisplayBoxSelected.png"))
+    dragDrop("outputSignalConnector.png", "imputSingnalConnector.png")
+    click(Pattern("playButton.png").similar(0.95))
+    wait(6)
+    assert(exists(Pattern("SignalDisplayWindow.png").similar(0.58)))
+    click("stopButton.png")
+    waitVanish(Pattern("SignalDisplayWindow.png").similar(0.58))
+             
+def tearDown(self):
+    App.close(self.terminal)
+
