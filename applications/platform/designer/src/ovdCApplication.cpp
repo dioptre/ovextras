34d82d52e8d66c46998e59fd0068875081f39d07
Guillermo Andrade B <guillermo.Andrade-Barroso@inria.fr>
2014-03-14 17:16:16 +0100

    add portable round fonction under visual studio 2008 and others c++ x11

--- applications/platform/designer/src/ovdCApplication.cpp
+++ applications/platform/designer/src/ovdCApplication.cpp
@@ -13,13 +13,22 @@
 #include <cstring>
 #include <cstdlib>
 #include <algorithm>
-#include <cmath>
+
+
+
+// round is defined in <cmath> on c++11
+inline int ov_round(double dbl)
+{ return dbl >= 0.0 ? (int)(dbl + 0.5) : ((dbl - (double)(int)dbl) <= -0.5 ? (int)dbl : (int)(dbl - 0.5));
+}
 
 #include <openvibe/ovITimeArithmetics.h>
 
 #if defined TARGET_OS_Linux
  #define _strcmpi strcasecmp
 #endif
+
+
+
 
 #define OVD_GUI_File          OpenViBE::Directories::getDataDir() + "/applications/designer/interface.ui"
 #define OVD_GUI_Settings_File OpenViBE::Directories::getDataDir() + "/applications/designer/interface-settings.ui"
@@ -1671,13 +1680,13 @@ void CApplication::aboutLinkClickedCB(const gchar *url)
 //Increase the zoom of the current scenario
 void CApplication::zoomInCB(void)
 {
-	gtk_spin_button_set_value(m_pZoomSpinner, round(getCurrentInterfacedScenario()->getScale()*100.0) + 5);
+	gtk_spin_button_set_value(m_pZoomSpinner, ov_round(getCurrentInterfacedScenario()->getScale()*100.0) + 5);
 }
-
+	
 //Decrease the zoom of the current scenario
 void CApplication::zoomOutCB(void)
 {
-	gtk_spin_button_set_value(m_pZoomSpinner, round(getCurrentInterfacedScenario()->getScale()*100.0) - 5);
+	gtk_spin_button_set_value(m_pZoomSpinner, ov_round(getCurrentInterfacedScenario()->getScale()*100.0) - 5);
 }
 
 void CApplication::spinnerZoomChangedCB(uint32 scalePercentage)
@@ -2219,7 +2228,7 @@ void CApplication::changeCurrentScenario(int32 i32PageIndex)
 		g_signal_connect(l_pWindowManagerButton, "toggled", G_CALLBACK(button_toggle_window_manager_cb), this);
 		
 		m_i32CurrentScenarioPage = i32PageIndex;
-		gtk_spin_button_set_value(m_pZoomSpinner, round(m_vInterfacedScenario[m_i32CurrentScenarioPage]->getScale()*100.0));
+		gtk_spin_button_set_value(m_pZoomSpinner, ov_round(m_vInterfacedScenario[m_i32CurrentScenarioPage]->getScale()*100.0));
 		updateWorkingDirectoryToken(m_vInterfacedScenario[m_i32CurrentScenarioPage]->m_oScenarioIdentifier);
 	}
 	//first scenario is created (or a scenario is opened and replaces first unnamed unmodified scenario)
