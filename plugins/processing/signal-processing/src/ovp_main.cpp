d0c8a05f82f10e5ac8b591b787e2f5d476e79e2c
Loic MAHE <loic.mahe@inria.fr>
2014-05-22 13:27:33 +0200

    removed material deprecated or related to features not ready for the release
    Former-commit-id: d85c329865bf69e49a3604c0ec5f6dc2145b81d8

--- plugins/processing/signal-processing/src/ovp_main.cpp
+++ plugins/processing/signal-processing/src/ovp_main.cpp
@@ -43,16 +43,18 @@
 #include "box-algorithms/basic/ovpCBoxAlgorithmARCoefficients.h"
 
 
-//Dieter boxes for p300
+/*/Dieter boxes for p300
 #include "box-algorithms/ovpCBoxAlgorithmTwoSampleTTest.h"
 #include "box-algorithms/ovpCBoxAlgorithmLikelinessDistributor.h"
-#include "box-algorithms/basic/ovpCBoxAlgorithmEpochVariance.h"
+
 #include "box-algorithms/ovpCBoxAlgorithmXDAWNSpatialFilterTrainer.h"
 #include "box-algorithms/ovpCBoxAlgorithmSpatialFilter.h"
 #include "box-algorithms/ovpCBoxAlgorithmMultipleSpatialFilters.h"
 #include "box-algorithms/ovpCBoxAlgorithmConditionalIdentity.h"
-#include "algorithms/basic/ovpCMatrixVariance.h"
+
 //*/
+#include "algorithms/basic/ovpCMatrixVariance.h"
+#include "box-algorithms/basic/ovpCBoxAlgorithmEpochVariance.h"
 
 OVP_Declare_Begin()
 
@@ -143,22 +145,25 @@ OVP_Declare_Begin()
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingBasic::CBoxAlgorithmHilbertDesc)
 
 
-	//Dieter stuff for p300
-rPluginModuleContext.getTypeManager().registerEnumerationType (OVP_TypeId_EpochAverageMethod, "Epoch Average method");
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Moving epoch average", OVP_TypeId_EpochAverageMethod_MovingAverage.toUInteger());
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Moving epoch average (Immediate)", OVP_TypeId_EpochAverageMethod_MovingAverageImmediate.toUInteger());
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Epoch block average", OVP_TypeId_EpochAverageMethod_BlockAverage.toUInteger());
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Cumulative average", OVP_TypeId_EpochAverageMethod_CumulativeAverage.toUInteger());
+	/*/Dieter stuff for p300
+
 
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingStatistics::CBoxAlgorithmTwoSampleTTestDesc);
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingStatistics::CBoxAlgorithmLikelinessDistributorDesc);
-	OVP_Declare_New(OpenViBEPlugins::SignalProcessing::CEpochVarianceDesc);
-	OVP_Declare_New(OpenViBEPlugins::SignalProcessing::CMatrixVarianceDesc);
+	
+	
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingCoAdapt::CBoxAlgorithmXDAWNSpatialFilterTrainerDesc);
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingCoAdapt::CBoxAlgorithmSpatialFilterDesc);
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingCoAdapt::CConditionalIdentityDesc);
 	OVP_Declare_New(OpenViBEPlugins::SignalProcessingCoAdapt::CBoxAlgorithmMultipleSpatialFiltersDesc);
 	//*/
+	rPluginModuleContext.getTypeManager().registerEnumerationType (OVP_TypeId_EpochAverageMethod, "Epoch Average method");
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Moving epoch average", OVP_TypeId_EpochAverageMethod_MovingAverage.toUInteger());
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Moving epoch average (Immediate)", OVP_TypeId_EpochAverageMethod_MovingAverageImmediate.toUInteger());
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Epoch block average", OVP_TypeId_EpochAverageMethod_BlockAverage.toUInteger());
+	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_EpochAverageMethod, "Cumulative average", OVP_TypeId_EpochAverageMethod_CumulativeAverage.toUInteger());
+	OVP_Declare_New(OpenViBEPlugins::SignalProcessing::CMatrixVarianceDesc);
+	OVP_Declare_New(OpenViBEPlugins::SignalProcessing::CEpochVarianceDesc);
 
 
 OVP_Declare_End()
