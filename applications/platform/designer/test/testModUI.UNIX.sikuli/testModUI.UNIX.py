3317a545671479b956ba66f5ceedaa761c47ca0e
Jussi Lindgren <openvibe-dev@inria.fr>
2014-07-09 15:32:24 +0200

    Tests: Changed Modifiable Settings test to reflect the new filenames
    Former-commit-id: 88b512ae7de0eba3e0fb39c516b6b91fac399f26

--- applications/platform/designer/test/testModUI.UNIX.sikuli/testModUI.UNIX.py
+++ applications/platform/designer/test/testModUI.UNIX.sikuli/testModUI.UNIX.py
@@ -3,7 +3,7 @@ def setUp(self):
     ov_binany_path=os.environ['OV_BINARY_PATH']
     self.terminal = App.open("xterm -e " +
              ov_binany_path +"/openvibe-designer.sh --no-session-management --open " +
-            ov_binany_path +"/share/openvibe/scenarios/box-tutorials/online-modifiable-temporal-filter.xml")
+            ov_binany_path +"/share/openvibe/scenarios/box-tutorials/modifiable-temporal-filter.xml")
     while not self.terminal.window():
         wait(1)
     wait("play.png",10)
