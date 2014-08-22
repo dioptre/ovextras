e295d9a54b5cfacee08867e14ce194576ff41756
Serrière Guillaume <guillaume.serriere@inria.fr>
2014-08-22 12:15:02 +0200

    Add mother class of SettingsView.
    Signed-off-by: Serrière Guillaume <guillaume.serriere@inria.fr>
    
    Former-commit-id: c9254ed6d47ffadfa66b266bc2aadf36ba476aa8

---
+++ applications/platform/designer/src/settings/ovdCAbstractSettingView.cpp
@@ -0,0 +1,31 @@
+#include "ovdCAbstractSettingView.h"
+
+using namespace OpenViBE;
+using namespace OpenViBEDesigner;
+using namespace OpenViBEDesigner::Setting;
+
+OpenViBE::CString CAbstractSettingView::OpenVigetSettingWidgetName(void)
+{
+	return m_sSettingWidgetName;
+}
+
+CAbstractSettingView::CAbstractSettingView(OpenViBE::Kernel::IBox& rBox, OpenViBE::uint32 ui32Index):
+	m_rBox(rBox), m_ui32Index(ui32Index), m_sSettingWidgetName("")
+{
+
+}
+
+OpenViBE::Kernel::IBox& CAbstractSettingView::getBox(void)
+{
+	return m_rBox;
+}
+
+OpenViBE::uint32 CAbstractSettingView::getSettingsIndex(void)
+{
+	return m_ui32Index;
+}
+
+void CAbstractSettingView::setSettingWidgetName(OpenViBE::CString & rWidgetName)
+{
+	m_sSettingWidgetName = rWidgetName;
+}
--- applications/platform/designer/src/settings/ovdCAbstractSettingView.h
+++ applications/platform/designer/src/settings/ovdCAbstractSettingView.h
@@ -1,4 +1,36 @@
 #ifndef OVDCABSTRACTSETTINGVIEW_H
 #define OVDCABSTRACTSETTINGVIEW_H
 
+#include "../ovd_base.h"
+
+namespace OpenViBEDesigner
+{
+	namespace Setting
+	{
+		class CAbstractSettingView{
+
+		public:
+			virtual OpenViBE::CString OpenVigetSettingWidgetName(void);
+
+			virtual void getValue(OpenViBE::CString &rValue) const = 0;
+			virtual void setValue(const OpenViBE::CString &rValue) = 0;
+
+		protected:
+			CAbstractSettingView(OpenViBE::Kernel::IBox& rBox, OpenViBE::uint32 ui32Index);
+
+			virtual OpenViBE::Kernel::IBox& getBox(void);
+
+			virtual OpenViBE::uint32 getSettingsIndex(void);
+
+			virtual void setSettingWidgetName(OpenViBE::CString & rWidgetName);
+
+		private:
+			OpenViBE::Kernel::IBox& m_rBox;
+			OpenViBE::uint32 m_ui32Index;
+			OpenViBE::CString m_sSettingWidgetName;
+		};
+	}
+
+}
+
 #endif // OVDCABSTRACTSETTINGVIEW_H
