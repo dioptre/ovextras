dioptre/ovextras

commit bc844393f9d1d857d1537972336b725f3843f544

Applications: Initial LSL output support as Acquisition Server plugin
Former-commit-id: 1f2b78521266fce9e33973bec62550b85c6c8afd

applications/platform/acquisition-server/src/ovasCAcquisitionServer.cpp
@@ -525,9 +525,20 @@ boolean CAcquisitionServer::loop(void)
 	// sends data to connected client(s)
 	while(m_vPendingBuffer.size() >= m_ui32SampleCountPerSentBlock*2)
 	{
-		uint64 start;
-		uint64 end;
+		const int64 p = m_ui64SampleCount-m_vPendingBuffer.size();
+		if (p < 0)
+			m_rKernelContext.getLogManager() << LogLevel_Error << "Signed number used for bit operations:" << p << " (case A)\n";
 
+		const uint64 bufferStart = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui64SampleCount-m_vPendingBuffer.size());
+		const uint64 bufferEnd   = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui64SampleCount-m_vPendingBuffer.size()+m_ui32SampleCountPerSentBlock);
+
+		// Pass the stimuli and buffer to all plugins; note that they may modify them
+		for(std::vector<IAcquisitionServerPlugin*>::iterator itp = m_vPlugins.begin(); itp != m_vPlugins.end(); ++itp)
+		{
+			(*itp)->loopHook(m_vPendingBuffer, m_oPendingStimulationSet, bufferStart, bufferEnd);
+		}
+
+		// Handle connections
 		for(itConnection=m_vConnection.begin(); itConnection!=m_vConnection.end(); itConnection++)
 		{
 			// Socket::IConnection* l_pConnection=itConnection->first;
@@ -548,43 +559,32 @@ boolean CAcquisitionServer::loop(void)
 					}
 				}
 
-				// Stimulation buffer
-				CStimulationSet l_oStimulationSet;
 				//				int l = l_oStimulationSet.getStimulationCount();
 
-				int64 p = m_ui64SampleCount-m_vPendingBuffer.size()+l_rInfo.m_ui64SignalSampleCountToSkip;
+				const int64 p = bufferStart+l_rInfo.m_ui64SignalSampleCountToSkip;
 				if (p < 0)
-					m_rKernelContext.getLogManager() << LogLevel_Error << "Signed number used for bit operations:" << p << "\n";
+					m_rKernelContext.getLogManager() << LogLevel_Error << "Signed number used for bit operations:" << p << " (case B)\n";
 
-				//l_rInfo.m_ui64SignalSampleCountToSkip = 0;
-
-				start = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, (m_ui64SampleCount-m_vPendingBuffer.size()+0                            ) + l_rInfo.m_ui64SignalSampleCountToSkip);
-				end   = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, (m_ui64SampleCount-m_vPendingBuffer.size()+m_ui32SampleCountPerSentBlock) + l_rInfo.m_ui64SignalSampleCountToSkip);
-
-				OpenViBEToolkit::Tools::StimulationSet::appendRange(
-							l_oStimulationSet,
-							m_oPendingStimulationSet,
-							start,end);
+				const uint64 connBlockStart = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferStart + l_rInfo.m_ui64SignalSampleCountToSkip);
+				const uint64 connBlockEnd   = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferEnd   + l_rInfo.m_ui64SignalSampleCountToSkip);
 
 				//m_rKernelContext.getLogManager() << LogLevel_Info << "start: " << time64(start) << "end: " << time64(end) << "\n";
 
-				for(std::vector<IAcquisitionServerPlugin*>::iterator itp = m_vPlugins.begin(); itp != m_vPlugins.end(); ++itp)
-				{
-					(*itp)->loopHook(l_oStimulationSet, start, end);
-				}
-				OpenViBEToolkit::Tools::StimulationSet::copy(*ip_pStimulationSet, l_oStimulationSet, -int64(l_rInfo.m_ui64StimulationTimeOffset));
+				// Stimulation buffer
+				CStimulationSet l_oStimulationSet;
 
+				// Take the stimuli range valid for the connection
+				OpenViBEToolkit::Tools::StimulationSet::appendRange(
+							l_oStimulationSet,
+							m_oPendingStimulationSet,
+							connBlockStart,connBlockEnd);
+
+				OpenViBEToolkit::Tools::StimulationSet::copy(*ip_pStimulationSet, l_oStimulationSet, -int64(l_rInfo.m_ui64StimulationTimeOffset));
 
 				op_pEncodedMemoryBuffer->setSize(0, true);
 				m_pStreamEncoder->process(OVP_GD_Algorithm_MasterAcquisitionStreamEncoder_InputTriggerId_EncodeBuffer);
 
-#if 0
-				uint64 l_ui64MemoryBufferSize=op_pEncodedMemoryBuffer->getSize();
-				l_pConnection->sendBufferBlocking(&l_ui64MemoryBufferSize, sizeof(l_ui64MemoryBufferSize));
-				l_pConnection->sendBufferBlocking(op_pEncodedMemoryBuffer->getDirectPointer(), (uint32)op_pEncodedMemoryBuffer->getSize());
-#else
 				l_rInfo.m_pConnectionClientHandlerThread->scheduleBuffer(*op_pEncodedMemoryBuffer);
-#endif
 			}
 			else
 			{
@@ -595,8 +595,8 @@ boolean CAcquisitionServer::loop(void)
 		// Clears pending stimulations
 		OpenViBEToolkit::Tools::StimulationSet::removeRange(
 					m_oPendingStimulationSet,
-					ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui64SampleCount-m_vPendingBuffer.size()),
-					ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui64SampleCount-m_vPendingBuffer.size()+m_ui32SampleCountPerSentBlock)
+					ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferStart),
+					ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferEnd)
 					);
 
 		// Clears pending signal
@@ -654,6 +654,13 @@ boolean CAcquisitionServer::connect(IDriver& rDriver, IHeader& rHeaderCopy, uint
 
 		for (OpenViBE::uint32 i=0;i<m_ui32ChannelCount;++i)
 			m_vSelectedChannels.push_back(i);
+	}
+	
+	// These are passed to plugins
+	m_vSelectedChannelNames.clear();
+	for(uint32 i=0;i<rHeaderCopy.getChannelCount();i++) 
+	{
+		m_vSelectedChannelNames.push_back(CString(rHeaderCopy.getChannelName(i)));
 	}
 
 	if(m_ui32ChannelCount==0)
@@ -789,7 +796,7 @@ boolean CAcquisitionServer::start(void)
 
 	for(std::vector<IAcquisitionServerPlugin*>::iterator itp = m_vPlugins.begin(); itp != m_vPlugins.end(); ++itp)
 	{
-		(*itp)->startHook();
+		(*itp)->startHook(m_vSelectedChannelNames, m_ui32SamplingFrequency, m_ui32ChannelCount, m_ui32SampleCountPerSentBlock);
 	}
 
 	m_bStarted=true;

applications/platform/acquisition-server/CMakeLists.txt
@@ -12,9 +12,23 @@ IF(PATH_OPENVIBE_CONTRIBUTIONS)
 	INCLUDE("${CMAKE_SOURCE_DIR}/contrib/common/contribAcquisitionServer.cmake")
 ENDIF(PATH_OPENVIBE_CONTRIBUTIONS)
 
+SET(ADDITIONAL_PATH "${CMAKE_SOURCE_DIR}/plugins/server-extensions/")
+FILE(GLOB_RECURSE additional_source_files ${ADDITIONAL_PATH}/*.cpp ${ADDITIONAL_PATH}/*.h)
+SET(source_files "${source_files};${additional_source_files}")
+
 ADD_EXECUTABLE(${PROJECT_NAME} ${source_files})
 INCLUDE_DIRECTORIES("src/drivers")
 INCLUDE_DIRECTORIES("include")
+
+# add include paths for non-contrib server plugins
+FILE(GLOB children ${ADDITIONAL_PATH}/*)
+FOREACH(FILENAME ${children})
+	# MESSAGE("Testing ${FILENAME}")
+	IF(IS_DIRECTORY ${FILENAME})
+		# MESSAGE("Adding ${FILENAME}")
+		INCLUDE_DIRECTORIES(${FILENAME})
+	ENDIF(IS_DIRECTORY ${FILENAME})
+ENDFOREACH(FILENAME)
 
 INCLUDE("FindOpenViBE")
 INCLUDE("FindOpenViBECommon")
@@ -35,6 +49,8 @@ INCLUDE("FindThirdPartyOpenAL")
 INCLUDE("FindThirdPartyBoost")
 INCLUDE("FindThirdPartyBoost_Thread")
 INCLUDE("FindThirdPartyPThreadsVCE2")	# for Windows Fieldtrip
+
+INCLUDE("FindThirdPartyLSL")
 
 IF(PATH_OPENVIBE_CONTRIBUTIONS)
 	INCLUDE("${CMAKE_SOURCE_DIR}/contrib/common/contribAcquisitionServerLinkLibs.cmake")

applications/platform/acquisition-server/include/ovasIAcquisitionServerPlugin.h
@@ -46,8 +46,9 @@ namespace OpenViBEAcquisitionServer
 			/// Hook called at the end of the AcquisitionServer constructor
 			virtual void createHook() {}
 
-			/// Hook called at the end of the start() function of AcquisitionServer
-			virtual void startHook() {}
+			/// Hook called at the end of the start() function of AcquisitionServer. At this point the device has been connected to,
+			/// and signal properties should already be correct.
+			virtual void startHook(const std::vector<OpenViBE::CString>& vSelectedChannelNames, OpenViBE::uint32 ui32SamplingFrequency, OpenViBE::uint32 ui32ChannelCount, OpenViBE::uint32 ui32SampleCountPerSentBlock) {}
 
 			/// Hook called at the end of the stop() function of AcquisitionServer
 			virtual void stopHook() {}
@@ -55,10 +56,13 @@ namespace OpenViBEAcquisitionServer
 
 			/** \brief Hook called in the loop() function of AcquisitionServer
 			  *
-			  * This hook is called after injecting the signal matrix and before sending the stimulations.
-			  * It gets a reference to the current stimulation set with its start and end dates.
+			  * This hook is called before sending the stimulations or signal to the connected clients.
+			  * It gets a reference to the current signal buffer and the stimulation set with its start and end dates.
 			  */
-			virtual void loopHook(OpenViBE::CStimulationSet&, OpenViBE::uint64, OpenViBE::uint64) {}
+			virtual void loopHook(std::vector < std::vector < OpenViBE::float32 > >& vPendingBuffer, 
+								  OpenViBE::CStimulationSet& oStimulationSet, 
+								  OpenViBE::uint64 start, 
+								  OpenViBE::uint64 end) {}
 
 			/// Hook called at the end of the acceptNewConnection() function of AcquisitionServer
 			virtual void acceptNewConnectionHook() {}

applications/platform/acquisition-server/src/ovasCAcquisitionServer.h
@@ -140,6 +140,7 @@ namespace OpenViBEAcquisitionServer
 		std::vector < OpenViBE::float32 > m_vOverSamplingSwapBuffer;
 		std::vector < OpenViBE::float64 > m_vImpedance;
 		std::vector < OpenViBE::uint32 >  m_vSelectedChannels;
+		std::vector < OpenViBE::CString >  m_vSelectedChannelNames;
 		Socket::IConnectionServer* m_pConnectionServer;
 
 		OpenViBEAcquisitionServer::EDriftCorrectionPolicy m_eDriftCorrectionPolicy;

applications/platform/acquisition-server/src/ovasCAcquisitionServerGUI.cpp
@@ -8,6 +8,7 @@
 #include "contribAcquisitionServer.inl"
 #endif
 
+#include "ovasCPluginLSLOutput.h"
 
 #include "generic-oscilator/ovasCDriverGenericOscilator.h"
 #include "generic-sawtooth/ovasCDriverGenericSawTooth.h"
@@ -164,6 +165,12 @@ CAcquisitionServerGUI::CAcquisitionServerGUI(const IKernelContext& rKernelContex
 
 #if defined TARGET_HAS_OpenViBEContributions
 	OpenViBEContributions::initiateContributions(this, m_pAcquisitionServer, rKernelContext, &m_vDriver);
+#endif
+
+	// Plugins that just send out data must be the last in list (since other plugins may modify the data)
+
+#if defined TARGET_HAS_ThirdPartyLSL
+	registerPlugin(new OpenViBEAcquisitionServer::OpenViBEAcquisitionServerPlugins::CPluginLSLOutput(rKernelContext));
 #endif
 
 	std::sort(m_vDriver.begin(), m_vDriver.end(), compare_driver_names);

contrib/plugins/server-extensions/external-stimulations/ovasCPluginExternalStimulations.cpp
@@ -49,7 +49,7 @@ CPluginExternalStimulations::~CPluginExternalStimulations()
 // Hooks
 
 
-void CPluginExternalStimulations::startHook()
+void CPluginExternalStimulations::startHook(const std::vector<OpenViBE::CString>& /*vSelectedChannelNames*/, OpenViBE::uint32 /* ui32SamplingFrequency */, OpenViBE::uint32 /* ui32ChannelCount */, OpenViBE::uint32 /* ui32SampleCountPerSentBlock */)
 {
 #ifdef OV_BOOST_SETTINGS
 	m_bIsExternalStimulationsEnabled = getSetting<boolean>("Enable External Stimulations");
@@ -77,7 +77,7 @@ void CPluginExternalStimulations::startHook()
 
 }
 
-void CPluginExternalStimulations::loopHook(CStimulationSet &stimulationSet, uint64 start, uint64 end)
+void CPluginExternalStimulations::loopHook(std::vector < std::vector < OpenViBE::float32 > >& /* vPendingBuffer */, CStimulationSet &stimulationSet, uint64 start, uint64 end)
 {
 	if (m_bIsExternalStimulationsEnabled)
 	{

contrib/plugins/server-extensions/external-stimulations/ovasCPluginExternalStimulations.h
@@ -29,9 +29,10 @@ namespace OpenViBEAcquisitionServer
 				CPluginExternalStimulations(const OpenViBE::Kernel::IKernelContext& rKernelContext);
 				virtual ~CPluginExternalStimulations();
 
-				virtual void startHook();
+				virtual void startHook(const std::vector<OpenViBE::CString>&, OpenViBE::uint32 ui32SamplingFrequency, OpenViBE::uint32 ui32ChannelCount, OpenViBE::uint32 ui32SampleCountPerSentBlock);
 				virtual void stopHook();
-				virtual void loopHook(OpenViBE::CStimulationSet& stimulationSet, OpenViBE::uint64 start, OpenViBE::uint64 end);
+				virtual void loopHook(std::vector < std::vector < OpenViBE::float32 > >& vPendingBuffer, 
+					OpenViBE::CStimulationSet& stimulationSet, OpenViBE::uint64 start, OpenViBE::uint64 end);
 				virtual void acceptNewConnectionHook();
 
 

plugins/server-extensions/lsl-output/ovasCPluginLSLOutput.cpp
@@ -1,0 +1,150 @@
+
+#if defined TARGET_HAS_ThirdPartyLSL
+
+#include "ovasCPluginLSLOutput.h"
+
+#include <vector>
+#include <iostream>
+
+#include <openvibe/ovITimeArithmetics.h>
+
+#include "../ovasCSettingsHelper.h"
+#include "../ovasCSettingsHelperOperators.h"
+
+// #define boolean OpenViBE::boolean
+
+using namespace OpenViBE;
+using namespace OpenViBE::Kernel;
+using namespace OpenViBEAcquisitionServer;
+using namespace OpenViBEAcquisitionServerPlugins;
+using namespace std;
+
+CPluginLSLOutput::CPluginLSLOutput(const IKernelContext& rKernelContext) :
+	IAcquisitionServerPlugin(rKernelContext, CString("AcquisitionServer_Plugin_LSLOutput")),
+	m_bIsLSLOutputEnabled(false),
+	m_sLSLOutputStreamName("openvibeLSLOutput"),
+	m_oSignalOutlet(NULL),
+	m_oStimulusOutlet(NULL),
+	m_ui32SampleCountPerSentBlock(0)
+{
+	m_rKernelContext.getLogManager() << LogLevel_Info << "Loading plugin: LSL Output\n";
+
+	m_oSettingsHelper.add("EnableLSLOutput", &m_bIsLSLOutputEnabled);
+	m_oSettingsHelper.add("LSLStreamName", &m_sLSLOutputStreamName);
+	m_oSettingsHelper.load();
+
+}
+
+CPluginLSLOutput::~CPluginLSLOutput()
+{
+	if(m_oSignalOutlet) 
+	{
+		delete m_oSignalOutlet;
+		m_oSignalOutlet = NULL;
+	}
+	if(m_oStimulusOutlet) 
+	{
+		delete m_oStimulusOutlet;
+		m_oStimulusOutlet = NULL;
+	}
+}
+
+// Hooks
+
+
+void CPluginLSLOutput::startHook(const std::vector<OpenViBE::CString>& vSelectedChannelNames, OpenViBE::uint32 ui32SamplingFrequency, OpenViBE::uint32 ui32ChannelCount, OpenViBE::uint32 ui32SampleCountPerSentBlock)
+{
+
+	m_ui32SampleCountPerSentBlock = ui32SampleCountPerSentBlock;
+
+	if (m_bIsLSLOutputEnabled)
+	{
+		// Open a signal stream 
+		const CString l_sIdentifier = m_sLSLOutputStreamName + "Signal"; 
+		lsl::stream_info l_oSignalInfo(m_sLSLOutputStreamName.toASCIIString(),"signal",ui32ChannelCount,ui32SamplingFrequency,lsl::cf_float32,l_sIdentifier.toASCIIString());
+
+		lsl::xml_element l_oChannels = l_oSignalInfo.desc().append_child("channels");
+
+		for (uint32 i=0;i<ui32ChannelCount;i++)
+		{
+			l_oChannels.append_child("channel")
+				.append_child_value("label",vSelectedChannelNames[i])
+				.append_child_value("unit","unknown")
+				.append_child_value("type","signal");
+		}
+
+		// make a new outlet
+		m_oSignalOutlet = new lsl::stream_outlet(l_oSignalInfo, m_ui32SampleCountPerSentBlock);
+
+		// Open a stimulus stream
+		const CString l_sStimulusIdentifier = m_sLSLOutputStreamName + "Markers"; 
+		lsl::stream_info l_oStimulusInfo(l_sStimulusIdentifier.toASCIIString(),"Markers",1,lsl::IRREGULAR_RATE,lsl::cf_string,l_sStimulusIdentifier.toASCIIString());
+
+		l_oStimulusInfo.desc().append_child("channels")
+						.append_child("channel")
+						.append_child_value("label","Stimulations")
+						.append_child_value("type","marker");
+
+		m_oStimulusOutlet = new lsl::stream_outlet(l_oStimulusInfo);
+
+		m_rKernelContext.getLogManager() << LogLevel_Info << "LSL Output activated...\n";
+	}
+}
+
+void CPluginLSLOutput::loopHook(std::vector < std::vector < OpenViBE::float32 > >& vPendingBuffer, CStimulationSet &stimulationSet, uint64 start, uint64 end)
+{
+	if (m_bIsLSLOutputEnabled)
+	{
+		// Output signal
+		if(m_oSignalOutlet->have_consumers())
+		{
+			for(uint32 i=0;i<m_ui32SampleCountPerSentBlock;i++)
+			{
+				m_oSignalOutlet->push_sample(vPendingBuffer[i]);
+			}
+		}
+
+		// Output stimuli
+		if(m_oStimulusOutlet->have_consumers())
+		{
+			for(uint32 i=0;i<stimulationSet.getStimulationCount();i++)
+			{	
+				if(stimulationSet.getStimulationDate(i) >= start &&
+				  stimulationSet.getStimulationDate(i) < end)
+				{
+					char l_sStimulationCode[128];
+					sprintf(l_sStimulationCode, "%ld", stimulationSet.getStimulationIdentifier(i));
+
+					const string l_sStimulationString(l_sStimulationCode);
+
+					const float64 l_f64StimulationDate = ITimeArithmetics::timeToSeconds(stimulationSet.getStimulationDate(i));
+
+					// std::cout << l_f64StimulationDate << "\n";
+
+					m_oStimulusOutlet->push_sample(&l_sStimulationString,l_f64StimulationDate);
+				}
+			}
+		}
+	}
+
+}
+
+void CPluginLSLOutput::stopHook()
+{
+	if (m_bIsLSLOutputEnabled)
+	{
+		if(m_oSignalOutlet)
+		{
+			delete m_oSignalOutlet;
+			m_oSignalOutlet = NULL;
+		}
+		if(m_oStimulusOutlet)
+		{
+			delete m_oStimulusOutlet;
+			m_oStimulusOutlet = NULL;
+		}
+	}
+}
+
+
+#endif

commit 06527e7071b90818a4bbb9427cdcb83bbeb64337

Server: Fixed issue with chunk start and end times
Bug was introduced in bc844393f9d1d857d1537972336b725f3843f544 [formerly 1f2b78521266fce9e33973bec62550b85c6c8afd]


Former-commit-id: 0093d6dbcd6f14388358019c89e2c169f7f5cc6a

applications/platform/acquisition-server/src/ovasCAcquisitionServer.cpp
@@ -401,24 +401,24 @@ boolean CAcquisitionServer::loop(void)
 				// acq server is started, send the header
 
 				// Computes inner data to skip
-				int64 l_i64SignedTheoricalSampleCountToSkip=0;
+				int64 l_i64SignedTheoreticalSampleCountToSkip=0;
 				if(m_bDriftCorrectionCalled)
 				{
-					l_i64SignedTheoricalSampleCountToSkip=((int64(itConnection->second.m_ui64ConnectionTime-m_ui64StartTime)*m_ui32SamplingFrequency)>>32)-m_ui64SampleCount+m_vPendingBuffer.size();
+					l_i64SignedTheoreticalSampleCountToSkip=((int64(itConnection->second.m_ui64ConnectionTime-m_ui64StartTime)*m_ui32SamplingFrequency)>>32)-m_ui64SampleCount+m_vPendingBuffer.size();
 				}
 				else
 				{
-					l_i64SignedTheoricalSampleCountToSkip=((int64(itConnection->second.m_ui64ConnectionTime-m_ui64LastDeliveryTime)*m_ui32SamplingFrequency)>>32)+m_vPendingBuffer.size();
+					l_i64SignedTheoreticalSampleCountToSkip=((int64(itConnection->second.m_ui64ConnectionTime-m_ui64LastDeliveryTime)*m_ui32SamplingFrequency)>>32)+m_vPendingBuffer.size();
 				}
 
-				uint64 l_ui64TheoricalSampleCountToSkip=(l_i64SignedTheoricalSampleCountToSkip<0?0:uint64(l_i64SignedTheoricalSampleCountToSkip));
+				uint64 l_ui64TheoreticalSampleCountToSkip=(l_i64SignedTheoreticalSampleCountToSkip<0?0:uint64(l_i64SignedTheoreticalSampleCountToSkip));
 
-				m_rKernelContext.getLogManager() << LogLevel_Trace << "Sample count offset at connection : " << l_ui64TheoricalSampleCountToSkip << "\n";
+				m_rKernelContext.getLogManager() << LogLevel_Trace << "Sample count offset at connection : " << l_ui64TheoreticalSampleCountToSkip << "\n";
 
 				SConnectionInfo l_oInfo;
 				l_oInfo.m_ui64ConnectionTime=itConnection->second.m_ui64ConnectionTime;
-				l_oInfo.m_ui64StimulationTimeOffset=ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui64TheoricalSampleCountToSkip+m_ui64SampleCount-m_vPendingBuffer.size());
-				l_oInfo.m_ui64SignalSampleCountToSkip=l_ui64TheoricalSampleCountToSkip;
+				l_oInfo.m_ui64StimulationTimeOffset=ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui64TheoreticalSampleCountToSkip+m_ui64SampleCount-m_vPendingBuffer.size());
+				l_oInfo.m_ui64SignalSampleCountToSkip=l_ui64TheoreticalSampleCountToSkip;
 				l_oInfo.m_pConnectionClientHandlerThread=new CConnectionClientHandlerThread(*this, *l_pConnection);
 				l_oInfo.m_pConnectionClientHandlerBoostThread=new boost::thread(boost::bind(&start_connection_client_handler_thread, l_oInfo.m_pConnectionClientHandlerThread));
 				//applyPriority(l_oInfo.m_pConnectionClientHandlerBoostThread,15);
@@ -529,13 +529,15 @@ boolean CAcquisitionServer::loop(void)
 		if (p < 0)
 			m_rKernelContext.getLogManager() << LogLevel_Error << "Signed number used for bit operations:" << p << " (case A)\n";
 
-		const uint64 bufferStart = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui64SampleCount-m_vPendingBuffer.size());
-		const uint64 bufferEnd   = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, m_ui64SampleCount-m_vPendingBuffer.size()+m_ui32SampleCountPerSentBlock);
+		const uint64 l_ui64BufferStartSamples = m_ui64SampleCount-m_vPendingBuffer.size();
+		const uint64 l_ui64BufferEndSamples   = m_ui64SampleCount-m_vPendingBuffer.size()+m_ui32SampleCountPerSentBlock;
+		const uint64 l_ui64BufferStartTime    = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui64BufferStartSamples);
+		const uint64 l_ui64BufferEndTime      = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui64BufferEndSamples);
 
 		// Pass the stimuli and buffer to all plugins; note that they may modify them
 		for(std::vector<IAcquisitionServerPlugin*>::iterator itp = m_vPlugins.begin(); itp != m_vPlugins.end(); ++itp)
 		{
-			(*itp)->loopHook(m_vPendingBuffer, m_oPendingStimulationSet, bufferStart, bufferEnd);
+			(*itp)->loopHook(m_vPendingBuffer, m_oPendingStimulationSet, l_ui64BufferStartTime, l_ui64BufferEndTime);
 		}
 
 		// Handle connections
@@ -561,12 +563,12 @@ boolean CAcquisitionServer::loop(void)
 
 				//				int l = l_oStimulationSet.getStimulationCount();
 
-				const int64 p = bufferStart+l_rInfo.m_ui64SignalSampleCountToSkip;
+				const int64 p = l_ui64BufferStartSamples+l_rInfo.m_ui64SignalSampleCountToSkip;
 				if (p < 0)
 					m_rKernelContext.getLogManager() << LogLevel_Error << "Signed number used for bit operations:" << p << " (case B)\n";
 
-				const uint64 connBlockStart = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferStart + l_rInfo.m_ui64SignalSampleCountToSkip);
-				const uint64 connBlockEnd   = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferEnd   + l_rInfo.m_ui64SignalSampleCountToSkip);
+				const uint64 l_ui64ConnBlockStartTime = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui64BufferStartSamples + l_rInfo.m_ui64SignalSampleCountToSkip);
+				const uint64 l_ui64ConnBlockEndTime   = ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, l_ui64BufferEndSamples   + l_rInfo.m_ui64SignalSampleCountToSkip);
 
 				//m_rKernelContext.getLogManager() << LogLevel_Info << "start: " << time64(start) << "end: " << time64(end) << "\n";
 
@@ -577,7 +579,7 @@ boolean CAcquisitionServer::loop(void)
 				OpenViBEToolkit::Tools::StimulationSet::appendRange(
 							l_oStimulationSet,
 							m_oPendingStimulationSet,
-							connBlockStart,connBlockEnd);
+							l_ui64ConnBlockStartTime,l_ui64ConnBlockEndTime);
 
 				OpenViBEToolkit::Tools::StimulationSet::copy(*ip_pStimulationSet, l_oStimulationSet, -int64(l_rInfo.m_ui64StimulationTimeOffset));
 
@@ -595,8 +597,8 @@ boolean CAcquisitionServer::loop(void)
 		// Clears pending stimulations
 		OpenViBEToolkit::Tools::StimulationSet::removeRange(
 					m_oPendingStimulationSet,
-					ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferStart),
-					ITimeArithmetics::sampleCountToTime(m_ui32SamplingFrequency, bufferEnd)
+					l_ui64BufferStartTime,
+					l_ui64BufferEndTime
 					);
 
 		// Clears pending signal
@@ -810,7 +812,7 @@ boolean CAcquisitionServer::stop(void)
 	m_rKernelContext.getLogManager() << LogLevel_Info << "Stopping the acquisition.\n";
 
 	int64 l_i64DriftSampleCount=m_i64DriftSampleCount-(m_i64DriftCorrectionSampleCountAdded-m_i64DriftCorrectionSampleCountRemoved);
-	uint64 l_ui64TheoricalSampleCount=m_ui64SampleCount-m_i64DriftSampleCount;
+	uint64 l_ui64TheoreticalSampleCount=m_ui64SampleCount-m_i64DriftSampleCount;
 	uint64 l_ui64ReceivedSampleCount=m_ui64SampleCount-(m_i64DriftCorrectionSampleCountAdded-m_i64DriftCorrectionSampleCountRemoved);
 	float64 l_f64DriftRatio=(l_ui64ReceivedSampleCount?((l_i64DriftSampleCount*10000)/int64(l_ui64ReceivedSampleCount))/100.:0);
 	float64 l_f64AddedRatio=(l_ui64ReceivedSampleCount?((m_i64DriftCorrectionSampleCountAdded*10000)/int64(l_ui64ReceivedSampleCount))/100.:0);
@@ -819,20 +821,20 @@ boolean CAcquisitionServer::stop(void)
 	{
 		m_rKernelContext.getLogManager() << LogLevel_Trace << "For information, after " << (((System::Time::zgetTime()-m_ui64StartTime) * 1000) >> 32) * .001f << " seconds we got the following statistics :\n";
 		m_rKernelContext.getLogManager() << LogLevel_Trace << "  Received : " << l_ui64ReceivedSampleCount << " samples\n";
-		m_rKernelContext.getLogManager() << LogLevel_Trace << "  Should have received : " << l_ui64TheoricalSampleCount << " samples\n";
+		m_rKernelContext.getLogManager() << LogLevel_Trace << "  Should have received : " << l_ui64TheoreticalSampleCount << " samples\n";
 		m_rKernelContext.getLogManager() << LogLevel_Trace << "  Drift was : " << l_i64DriftSampleCount << " samples (" << l_f64DriftRatio << "%)\n";
 		m_rKernelContext.getLogManager() << LogLevel_Trace << "  Added : " << m_i64DriftCorrectionSampleCountAdded << " samples (" << l_f64AddedRatio << "%)\n";
 		m_rKernelContext.getLogManager() << LogLevel_Trace << "  Removed : " << m_i64DriftCorrectionSampleCountRemoved << " samples (" << l_f64RemovedRatio << "%)\n";
 	}
 	else
 	{
-		m_rKernelContext.getLogManager() << LogLevel_Warning << "After " << (((System::Time::zgetTime()-m_ui64StartTime) * 1000) >> 32) * .001f << " seconds, theorical samples per second does not match real samples per second\n";
+		m_rKernelContext.getLogManager() << LogLevel_Warning << "After " << (((System::Time::zgetTime()-m_ui64StartTime) * 1000) >> 32) * .001f << " seconds, theoretical samples per second does not match real samples per second\n";
 		m_rKernelContext.getLogManager() << LogLevel_Warning << "  Received : " << l_ui64ReceivedSampleCount << " samples\n";
-		m_rKernelContext.getLogManager() << LogLevel_Warning << "  Should have received : " << l_ui64TheoricalSampleCount << " samples\n";
+		m_rKernelContext.getLogManager() << LogLevel_Warning << "  Should have received : " << l_ui64TheoreticalSampleCount << " samples\n";
 		m_rKernelContext.getLogManager() << LogLevel_Warning << "  Drift was : " << l_i64DriftSampleCount << " samples (" << l_f64DriftRatio << "%)\n";
 		if(m_i64DriftCorrectionSampleCountAdded==0 && m_i64DriftCorrectionSampleCountRemoved==0)
 		{
-			m_rKernelContext.getLogManager() << LogLevel_ImportantWarning << "  The driver did not try to correct this difference\n";
+			m_rKernelContext.getLogManager() << LogLevel_ImportantWarning << "  The driver did not try to correct this difference. This may be a feature of the driver.\n";
 		}
 		else
 		{
@@ -840,7 +842,7 @@ boolean CAcquisitionServer::stop(void)
 			m_rKernelContext.getLogManager() << LogLevel_Warning << "  Removed : " << m_i64DriftCorrectionSampleCountRemoved << " samples (" << l_f64RemovedRatio << "%)\n";
 			m_rKernelContext.getLogManager() << LogLevel_Warning << "  The driver obviously tried to correct this difference\n";
 		}
-		m_rKernelContext.getLogManager() << LogLevel_ImportantWarning << "  Please submit a bug report (including the acquisition server log file or at least this complete message) for the driver you are using\n";
+		// m_rKernelContext.getLogManager() << LogLevel_ImportantWarning << "  Please submit a bug report (including the acquisition server log file or at least this complete message) for the driver you are using\n";
 	}
 
 	// Stops driver
@@ -983,8 +985,8 @@ void CAcquisitionServer::setSamples(const float32* pSample, const uint32 ui32Sam
 		m_ui64SampleCount+=ui32SampleCount*m_ui64OverSamplingFactor;
 
 		{
-			uint64 l_ui64TheoricalSampleCount=(m_ui32SamplingFrequency * (System::Time::zgetTime()-m_ui64StartTime))>>32;
-			int64 l_i64JitterSampleCount=int64(m_ui64SampleCount-l_ui64TheoricalSampleCount)+m_i64InnerLatencySampleCount;
+			uint64 l_ui64TheoreticalSampleCount=(m_ui32SamplingFrequency * (System::Time::zgetTime()-m_ui64StartTime))>>32;
+			int64 l_i64JitterSampleCount=int64(m_ui64SampleCount-l_ui64TheoreticalSampleCount)+m_i64InnerLatencySampleCount;
 
 			m_vJitterSampleCount.push_back(l_i64JitterSampleCount);
 			if(m_vJitterSampleCount.size() > m_ui64JitterEstimationCountForDrift)