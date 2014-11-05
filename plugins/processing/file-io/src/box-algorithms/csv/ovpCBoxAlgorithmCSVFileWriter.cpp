dioptre/ovextras

commit 0de81b15d7f7b1ddd0cc68c238dcb2e06eca74b9

Plugins: Various improvements to CSV Writer
Added support for Streamed Matrix
Added support for Feature Vector
Refactored to use the time arithmetics class for conversions
Removed unused Compression setting
Introduced setting to change the output precision


Former-commit-id: 92f65e5c617913fff10f5b2e6a7cf03113c037a4

plugins/processing/file-io/src/box-algorithms/csv/ovpCBoxAlgorithmCSVFileWriter.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 
+#include "openvibe/ovITimeArithmetics.h"
+
 using namespace OpenViBE;
 using namespace OpenViBE::Kernel;
 using namespace OpenViBE::Plugins;
@@ -11,8 +13,8 @@ using namespace OpenViBEPlugins;
 using namespace OpenViBEPlugins::FileIO;
 
 CBoxAlgorithmCSVFileWriter::CBoxAlgorithmCSVFileWriter(void)
-	:m_pFile(NULL)
-	,m_fpRealProcess(NULL)
+	:
+	m_fpRealProcess(NULL)
 	,m_pStreamDecoder(NULL)
 	,m_pMatrix(NULL)
 	,m_bDeleteMatrix(false)
@@ -23,16 +25,19 @@ boolean CBoxAlgorithmCSVFileWriter::initialize(void)
 {
 	this->getStaticBoxContext().getInputType(0, m_oTypeIdentifier);
 
-	CString l_sFilename=FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 0);
+	const CString l_sFilename=FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 0);
 	m_sSeparator=FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 1);
-	m_bUseCompression=FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 2);
+	const uint64 l_ui64Precision=FSettingValueAutoCast(*this->getBoxAlgorithmContext(), 2);
 
-	m_pFile=::fopen(l_sFilename.toASCIIString(), "wb");
-	if(!m_pFile)
+	m_oFileStream.open(l_sFilename.toASCIIString(), std::ios::trunc);
+	if(!m_oFileStream)
 	{
-		this->getLogManager() << LogLevel_ImportantWarning << "Could not open file [" << l_sFilename << "]\n";
+		this->getLogManager() << LogLevel_ImportantWarning << "Could not open file [" << l_sFilename << "] for writing\n";
 		return false;
 	}
+
+	m_oFileStream << std::scientific;
+	m_oFileStream.precision(static_cast<std::streamsize>(l_ui64Precision));
 
 	if(this->getTypeManager().isDerivedFromStream(m_oTypeIdentifier, OV_TypeId_StreamedMatrix))
 	{
@@ -48,8 +53,17 @@ boolean CBoxAlgorithmCSVFileWriter::initialize(void)
 			m_pStreamDecoder->initialize();
 			op_pMinMaxFrequencyBand.initialize(m_pStreamDecoder->getOutputParameter(OVP_GD_Algorithm_SpectrumStreamDecoder_OutputParameterId_MinMaxFrequencyBands));
 		}
+		else if(m_oTypeIdentifier==OV_TypeId_FeatureVector)
+		{
+			m_pStreamDecoder=&this->getAlgorithmManager().getAlgorithm(this->getAlgorithmManager().createAlgorithm(OVP_GD_ClassId_Algorithm_FeatureVectorStreamDecoder));
+			m_pStreamDecoder->initialize();
+		}
 		else
 		{
+			if(m_oTypeIdentifier!=OV_TypeId_StreamedMatrix)
+			{
+				this->getLogManager() << LogLevel_Info << "Input is a type derived from matrix that the box doesn't recognize, decoding as Streamed Matrix\n";
+			}
 			m_pStreamDecoder=&this->getAlgorithmManager().getAlgorithm(this->getAlgorithmManager().createAlgorithm(OVP_GD_ClassId_Algorithm_StreamedMatrixStreamDecoder));
 			m_pStreamDecoder->initialize();
 		}
@@ -72,10 +86,7 @@ boolean CBoxAlgorithmCSVFileWriter::initialize(void)
 		return false;
 	}
 
-	if(m_bUseCompression)
-	{
-		this->getLogManager() << LogLevel_Warning << "Compression flag not used yet, the file will be flagged uncompressed and stored as is\n";
-	}
+	m_ui64SampleCount = 0;
 
 	m_bFirstBuffer=true;
 	return true;
@@ -83,14 +94,15 @@ boolean CBoxAlgorithmCSVFileWriter::initialize(void)
 
 boolean CBoxAlgorithmCSVFileWriter::uninitialize(void)
 {
-	if(m_pFile)
+	if(m_oFileStream.is_open())
 	{
-		::fclose(m_pFile);
-		m_pFile=NULL;
+		m_oFileStream.close();
 	}
+
 	if(m_bDeleteMatrix)
 	{
 		delete m_pMatrix;
+		m_pMatrix = NULL;
 	}
 	op_pStimulationSet.uninitialize();
 	op_pMatrix.uninitialize();
@@ -122,36 +134,52 @@ boolean CBoxAlgorithmCSVFileWriter::process_streamedMatrix(void)
 	IBoxIO& l_rDynamicBoxContext=this->getDynamicBoxContext();
 	for(uint32 i=0; i<l_rDynamicBoxContext.getInputChunkCount(0); i++)
 	{
-		uint64 l_ui64StartTime=l_rDynamicBoxContext.getInputChunkStartTime(0, i);
-		uint64 l_ui64EndTime=l_rDynamicBoxContext.getInputChunkEndTime(0, i);
+		const uint64 l_ui64StartTime=l_rDynamicBoxContext.getInputChunkStartTime(0, i);
+		const uint64 l_ui64EndTime=l_rDynamicBoxContext.getInputChunkEndTime(0, i);
 		ip_pMemoryBuffer=l_rDynamicBoxContext.getInputChunk(0, i);
 		m_pStreamDecoder->process();
 		if(m_pStreamDecoder->isOutputTriggerActive(OVP_GD_Algorithm_StreamedMatrixStreamDecoder_OutputTriggerId_ReceivedHeader))
 		{
 			if(op_pMatrix->getDimensionCount() > 2 || op_pMatrix->getDimensionCount() < 1)
 			{
-				this->getLogManager() << LogLevel_ImportantWarning << "Input matrix does not have 1 or 2 dimensions - Could not write content in CSV file...\n";
+				this->getLogManager() << LogLevel_ImportantWarning << "Input matrix does not have 1 or 2 dimensions - Cannot write content in CSV file...\n";
 				return false;
 			}
 
+			// The matrix is a vector, make a [n x 1] matrix to represent it
 			if( op_pMatrix->getDimensionCount() == 1 )
 			{
 				m_pMatrix = new CMatrix();
 				m_bDeleteMatrix = true;
 				m_pMatrix->setDimensionCount(2);
-				m_pMatrix->setDimensionSize(0,1);
-				m_pMatrix->setDimensionSize(1,op_pMatrix->getDimensionSize(0));
-				for(uint32 i=0;i<op_pMatrix->getDimensionSize(0);i++)
+				if(m_oTypeIdentifier==OV_TypeId_FeatureVector)
 				{
-					m_pMatrix->setDimensionLabel(1,i,op_pMatrix->getDimensionLabel(0,i));
+					// Flip, [n channels X 1 sample]
+					m_pMatrix->setDimensionSize(0,op_pMatrix->getDimensionSize(0));
+					m_pMatrix->setDimensionSize(1,1);
+					for(uint32 i=0;i<op_pMatrix->getDimensionSize(0);i++)
+					{
+						m_pMatrix->setDimensionLabel(0,i,op_pMatrix->getDimensionLabel(0,i));
+					}
+				} 
+				else
+				{
+					// As-is, [1 channel X n samples]
+					m_pMatrix->setDimensionSize(0,1);
+					m_pMatrix->setDimensionSize(1,op_pMatrix->getDimensionSize(0));
+					for(uint32 i=0;i<op_pMatrix->getDimensionSize(0);i++)
+					{
+						m_pMatrix->setDimensionLabel(1,i,op_pMatrix->getDimensionLabel(0,i));
+					}
 				}
+
 			}
 			else
 			{
 				m_pMatrix=op_pMatrix;
 			}
 //			std::cout<<&m_pMatrix<<" "<<&op_pMatrix<<"\n";
-			::fprintf(m_pFile, "Time (s)");
+			m_oFileStream << "Time (s)";
 			for(uint32 c=0; c<m_pMatrix->getDimensionSize(0); c++)
 			{
 				std::string l_sLabel(m_pMatrix->getDimensionLabel(0, c));
@@ -159,68 +187,60 @@ boolean CBoxAlgorithmCSVFileWriter::process_streamedMatrix(void)
 				{
 					l_sLabel.erase(l_sLabel.length()-1);
 				}
-				::fprintf(m_pFile,
-					"%s%s",
-					m_sSeparator.toASCIIString(),
-					l_sLabel.c_str());
+				m_oFileStream << m_sSeparator.toASCIIString() << l_sLabel.c_str();
 			}
 
 			if(m_oTypeIdentifier==OV_TypeId_Signal)
 			{
-				::fprintf(m_pFile,
-					"%sSampling Rate",
-					m_sSeparator.toASCIIString());
+				m_oFileStream << m_sSeparator.toASCIIString() << "Sampling Rate";
 			}
 			else if(m_oTypeIdentifier==OV_TypeId_Spectrum)
 			{
-				::fprintf(m_pFile,
-					"%sMin frequency band",
-					m_sSeparator.toASCIIString());
-				::fprintf(m_pFile,
-					"%sMax frequency band",
-					m_sSeparator.toASCIIString());
+				m_oFileStream << m_sSeparator << "Min frequency band";
+				m_oFileStream << m_sSeparator << "Max frequency band";
 			}
 			else
 			{
 			}
 
-			::fprintf(m_pFile, "\n");
+			m_oFileStream << "\n";
 		}
 		if(m_pStreamDecoder->isOutputTriggerActive(OVP_GD_Algorithm_StreamedMatrixStreamDecoder_OutputTriggerId_ReceivedBuffer))
 		{
-			for(uint32 s=0; s<m_pMatrix->getDimensionSize(1); s++)
+			const uint32 l_ui32NumChannels = m_pMatrix->getDimensionSize(0);
+			const uint32 l_ui32NumSamples = m_pMatrix->getDimensionSize(1);
+
+			for(uint32 s=0; s<l_ui32NumSamples; s++)
 			{
-				if(m_oTypeIdentifier==OV_TypeId_Signal)   ::fprintf(m_pFile, "%f", ((l_ui64StartTime+((s*(l_ui64EndTime-l_ui64StartTime))/op_pMatrix->getDimensionSize(1)))>>16)/65536.);
-				if(m_oTypeIdentifier==OV_TypeId_Spectrum) ::fprintf(m_pFile, "%f", (l_ui64EndTime>>16)/65536.);
-				for(uint32 c=0; c<m_pMatrix->getDimensionSize(0); c++)
+				if(m_oTypeIdentifier==OV_TypeId_StreamedMatrix || m_oTypeIdentifier==OV_TypeId_FeatureVector)
 				{
-					::fprintf(m_pFile,
-						"%s%f",
-						m_sSeparator.toASCIIString(),
-						op_pMatrix->getBuffer()[c*m_pMatrix->getDimensionSize(1)+s]);
+					m_oFileStream << ITimeArithmetics::timeToSeconds(l_ui64StartTime);
+				}
+				else if(m_oTypeIdentifier==OV_TypeId_Signal)
+				{
+					m_oFileStream << ITimeArithmetics::timeToSeconds(ITimeArithmetics::sampleCountToTime(static_cast<uint64>(op_ui64SamplingFrequency), m_ui64SampleCount+s));
+				}
+				else if(m_oTypeIdentifier==OV_TypeId_Spectrum) 
+				{
+					m_oFileStream << ITimeArithmetics::timeToSeconds(l_ui64EndTime);
+				}
+				for(uint32 c=0; c<l_ui32NumChannels; c++)
+				{
+					m_oFileStream << m_sSeparator.toASCIIString() << op_pMatrix->getBuffer()[c*l_ui32NumSamples+s];
 				}
 
 				if(m_bFirstBuffer)
 				{
 					if(m_oTypeIdentifier==OV_TypeId_Signal)
 					{
-						::fprintf(m_pFile,
-							"%s%lli",
-							m_sSeparator.toASCIIString(),
-							(uint64)op_ui64SamplingFrequency);
+						m_oFileStream << m_sSeparator.toASCIIString() << (uint64)op_ui64SamplingFrequency;
 
 						m_bFirstBuffer=false;
 					}
 					else if(m_oTypeIdentifier==OV_TypeId_Spectrum)
 					{
-						::fprintf(m_pFile,
-							"%s%f",
-							m_sSeparator.toASCIIString(),
-							op_pMinMaxFrequencyBand->getBuffer()[s*2+0]);
-						::fprintf(m_pFile,
-							"%s%f",
-							m_sSeparator.toASCIIString(),
-							op_pMinMaxFrequencyBand->getBuffer()[s*2+1]);
+						m_oFileStream << m_sSeparator.toASCIIString() << op_pMinMaxFrequencyBand->getBuffer()[s*2+0];
+						m_oFileStream << m_sSeparator.toASCIIString() << op_pMinMaxFrequencyBand->getBuffer()[s*2+1];
 					}
 					else
 					{
@@ -230,26 +250,20 @@ boolean CBoxAlgorithmCSVFileWriter::process_streamedMatrix(void)
 				{
 					if(m_oTypeIdentifier==OV_TypeId_Signal)
 					{
-						::fprintf(m_pFile,
-							"%s",
-							m_sSeparator.toASCIIString());
+						m_oFileStream << m_sSeparator.toASCIIString();
 					}
 					else if(m_oTypeIdentifier==OV_TypeId_Spectrum)
 					{
-						::fprintf(m_pFile,
-							"%s",
-							m_sSeparator.toASCIIString());
-						::fprintf(m_pFile,
-							"%s",
-							m_sSeparator.toASCIIString());
+						m_oFileStream << m_sSeparator.toASCIIString() << m_sSeparator.toASCIIString();
 					}
 					else
 					{
 					}
 				}
 
-				::fprintf(m_pFile, "\n");
+				m_oFileStream << "\n";
 			}
+			m_ui64SampleCount += l_ui32NumSamples;
 
 			m_bFirstBuffer=false;
 		}
@@ -272,22 +286,18 @@ boolean CBoxAlgorithmCSVFileWriter::process_stimulation(void)
 		m_pStreamDecoder->process();
 		if(m_pStreamDecoder->isOutputTriggerActive(OVP_GD_Algorithm_StimulationStreamDecoder_OutputTriggerId_ReceivedHeader))
 		{
-			::fprintf(m_pFile,
-				"Time (s)%sIdentifier%sDuration\n",
-				m_sSeparator.toASCIIString(),
-				m_sSeparator.toASCIIString());
+			m_oFileStream << "Time (s)" << m_sSeparator.toASCIIString() << "Identifier" << m_sSeparator.toASCIIString() << "Duration\n";
 		}
 		if(m_pStreamDecoder->isOutputTriggerActive(OVP_GD_Algorithm_StimulationStreamDecoder_OutputTriggerId_ReceivedBuffer))
 		{
 			for(uint32 j=0; j<op_pStimulationSet->getStimulationCount(); j++)
 			{
-				::fprintf(m_pFile,
-					"%f%s%llu%s%f\n",
-					(op_pStimulationSet->getStimulationDate(j)>>16)/65536.0,
-					m_sSeparator.toASCIIString(),
-					op_pStimulationSet->getStimulationIdentifier(j),
-					m_sSeparator.toASCIIString(),
-					(op_pStimulationSet->getStimulationDuration(j)>>16)/65536.0);
+				m_oFileStream << ITimeArithmetics::timeToSeconds(op_pStimulationSet->getStimulationDate(j))
+					<< m_sSeparator.toASCIIString() 
+					<< op_pStimulationSet->getStimulationIdentifier(j)
+					<< m_sSeparator.toASCIIString() 
+					<< ITimeArithmetics::timeToSeconds(op_pStimulationSet->getStimulationDuration(j))
+					<< "\n";
 			}
 		}
 		if(m_pStreamDecoder->isOutputTriggerActive(OVP_GD_Algorithm_StimulationStreamDecoder_OutputTriggerId_ReceivedEnd))

applications/examples/convert/share/dat2csv.xml
@@ -165,7 +165,7 @@
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x4e7b798a, 0x183beafb)</Identifier>
-					<Value>(0x15b589dd, 0xe6ec3b92)</Value>
+					<Value>(0x229d1207, 0xebac8ab0)</Value>
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x527ad68d, 0x16d746a0)</Identifier>

applications/examples/convert/share/gdf2csv.xml
@@ -221,11 +221,12 @@
 					<Value>;</Value>
 				</Setting>
 				<Setting>
-					<TypeIdentifier>(0x2cdb2f0b, 0x12f231ea)</TypeIdentifier>
-					<Name>Use compression</Name>
-					<DefaultValue>true</DefaultValue>
-					<Value>true</Value>
-				</Setting>
+					<TypeIdentifier>(0x007deef9, 0x2f3e95c6)</TypeIdentifier>
+					<Name>Precision</Name>
+					<DefaultValue>10</DefaultValue>
+					<Value>10</Value>
+					<Modifiability>false</Modifiability>
+				</Setting>				
 			</Settings>
 			<Attributes>
 				<Attribute>
@@ -242,7 +243,7 @@
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x4e7b798a, 0x183beafb)</Identifier>
-					<Value>(0x15b589dd, 0xe6ec3b92)</Value>
+					<Value>(0x229d1207, 0xebac8ab0)</Value>
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x527ad68d, 0x16d746a0)</Identifier>

applications/examples/convert/share/ov2csv.xml
@@ -24,11 +24,12 @@
 					<Value>;</Value>
 				</Setting>
 				<Setting>
-					<TypeIdentifier>(0x2cdb2f0b, 0x12f231ea)</TypeIdentifier>
-					<Name>Use compression</Name>
-					<DefaultValue>true</DefaultValue>
-					<Value>true</Value>
-				</Setting>
+					<TypeIdentifier>(0x007deef9, 0x2f3e95c6)</TypeIdentifier>
+					<Name>Precision</Name>
+					<DefaultValue>10</DefaultValue>
+					<Value>10</Value>
+					<Modifiability>false</Modifiability>
+				</Setting>				
 			</Settings>
 			<Attributes>
 				<Attribute>
@@ -45,7 +46,7 @@
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x4e7b798a, 0x183beafb)</Identifier>
-					<Value>(0x15b589dd, 0xe6ec3b92)</Value>
+					<Value>(0x229d1207, 0xebac8ab0)</Value>
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x527ad68d, 0x16d746a0)</Identifier>

applications/examples/convert/share/vhdr2csv.xml
@@ -207,11 +207,12 @@
 					<Value>;</Value>
 				</Setting>
 				<Setting>
-					<TypeIdentifier>(0x2cdb2f0b, 0x12f231ea)</TypeIdentifier>
-					<Name>Use compression</Name>
-					<DefaultValue>true</DefaultValue>
-					<Value>true</Value>
-				</Setting>
+					<TypeIdentifier>(0x007deef9, 0x2f3e95c6)</TypeIdentifier>
+					<Name>Precision</Name>
+					<DefaultValue>10</DefaultValue>
+					<Value>10</Value>
+					<Modifiability>false</Modifiability>
+				</Setting>				
 			</Settings>
 			<Attributes>
 				<Attribute>
@@ -228,7 +229,7 @@
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x4e7b798a, 0x183beafb)</Identifier>
-					<Value>(0x15b589dd, 0xe6ec3b92)</Value>
+					<Value>(0x229d1207, 0xebac8ab0)</Value>
 				</Attribute>
 				<Attribute>
 					<Identifier>(0x527ad68d, 0x16d746a0)</Identifier>

applications/platform/designer/src/ovdCConnectorEditor.cpp
@@ -68,7 +68,7 @@ boolean CConnectorEditor::run(void)
 	gtk_window_set_title(GTK_WINDOW(l_pConnectorDialog), m_sTitle.c_str());
 
 	//get a list of stream types and display connector type
-	map<string, CIdentifier> m_vStreamTypes;
+	map<string, CIdentifier> l_vStreamTypes;
 	CIdentifier l_oCurrentTypeIdentifier;
 	gint l_iActive=-1;
 
@@ -83,10 +83,10 @@ boolean CConnectorEditor::run(void)
 				gtk_combo_box_append_text(l_pConnectorTypeComboBox, m_rKernelContext.getTypeManager().getTypeName(l_oCurrentTypeIdentifier).toASCIIString());
 				if(l_oCurrentTypeIdentifier==l_oConnectorType)
 				{
-					l_iActive=m_vStreamTypes.size();
+					l_iActive=l_vStreamTypes.size();
 					gtk_combo_box_set_active(l_pConnectorTypeComboBox, l_iActive);
 				}
-				m_vStreamTypes[m_rKernelContext.getTypeManager().getTypeName(l_oCurrentTypeIdentifier).toASCIIString()]=l_oCurrentTypeIdentifier;
+				l_vStreamTypes[m_rKernelContext.getTypeManager().getTypeName(l_oCurrentTypeIdentifier).toASCIIString()]=l_oCurrentTypeIdentifier;
 			}
 		}
 	}
@@ -104,7 +104,7 @@ boolean CConnectorEditor::run(void)
 			char* l_sActiveText=gtk_combo_box_get_active_text(l_pConnectorTypeComboBox);
 			if(l_sActiveText)
 			{
-				(m_rBox.*setConnectorType)(m_ui32ConnectorIndex, m_vStreamTypes[l_sActiveText]);
+				(m_rBox.*setConnectorType)(m_ui32ConnectorIndex, l_vStreamTypes[l_sActiveText]);
 				(m_rBox.*setConnectorName)(m_ui32ConnectorIndex, gtk_entry_get_text(l_pConnectorNameEntry));
 				l_bFinished=true;
 				l_bResult=true;

plugins/processing/file-io/doc/Doc_BoxAlgorithm_CSVFileWriter.dox-part
@@ -23,7 +23,8 @@ __________________________________________________________________
  * |OVP_DocBegin_BoxAlgorithm_CSVFileWriter_Input1|
 This is input is used to know what data to dump in the considered file. The type of
 this input can be modified by the user and the format of the output will be adapted
-depending on this type. Supported inputs are : Signal, Spectrum and Stimulations.
+depending on this type. Supported inputs are : Signal, Spectrum, Streamed Matrix,
+Feature Vector, and Stimulations.
 Format specific to each of these stream types is detailed in section
 \ref Doc_BoxAlgorithm_CSVFileWriter_Miscellaneous.
  * |OVP_DocEnd_BoxAlgorithm_CSVFileWriter_Input1|
@@ -45,9 +46,7 @@ fields. Typical examples are colon, semi colon and tabulations.
  * |OVP_DocEnd_BoxAlgorithm_CSVFileWriter_Setting2|
 
  * |OVP_DocBegin_BoxAlgorithm_CSVFileWriter_Setting3|
-Due to the fact that text files can grow very quickly, it is possible to compress the
-file using gzip format. If you want to use compression, just tick this setting.
-<b>Note: this is not implemented at the moment</b>.
+This allows you to set std::precision, i.e. how many digits are used to represent each input value in the output.
  * |OVP_DocEnd_BoxAlgorithm_CSVFileWriter_Setting3|
 __________________________________________________________________
 
@@ -69,9 +68,30 @@ For the Signal stream, you will have something like this :
 \verbatim
 Time (s), Channel 1, Channel 2, ..., Channel n, Sampling Rate
 Time of measure 1, Measure 1 of channel 1, Measure 1 of channel 2, ..., Measure 1 of channel n, Value of the sampling rate
-Time of measure 2, Measure 2 of channel 1, Measure 2 of channel 2, ..., Measure 2 of channel n
+Time of measure 2, Measure 2 of channel 1, Measure 2 of channel 2, ..., Measure 2 of channel n,
 ...
-Time of measure t, Measure t of channel 1, Measure t of channel 2, ..., Measure t of channel n
+Time of measure t, Measure t of channel 1, Measure t of channel 2, ..., Measure t of channel n,
+\endverbatim
+
+For the Streamed Matrix stream, you will have something like this :
+\verbatim
+Time (s), Column 1, Column 2, ..., Column n
+Time of matrix 1, slot [1,1] of 1, slot[1,2] of 1, ..., slot[1,n] of 1
+Time of matrix 1, slot [2,1] of 1, slot[2,2] of 1, ..., slot[2,n] of 1
+...
+Time of matrix 1, slot [m,1] of 1, slot[m,2] of 1, ..., slot[m,n] of 1
+...
+Time of matrix 2, slot [1,1] of 2, slot[1,2] of 2, ..., slot[1,n] of 2
+...
+\endverbatim
+
+For the Feature Vector stream, you will have something like this :
+\verbatim
+Time (s), Feature 1, Feature 2, ..., Feature n
+Time of vector 1, feature 1, feature 2, ..., feature n
+Time of vector 2, feature 1, feature 2, ..., feature n
+...
+Time of vector k, feature 1, feature 2, ..., feature n
 \endverbatim
 
 For the Spectrum stream, you will have something like this :

plugins/processing/file-io/src/box-algorithms/csv/ovpCBoxAlgorithmCSVFileWriter.h
@@ -7,6 +7,8 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <iostream>
+#include <fstream>
 
 // TODO:
 // - please move the identifier definitions in ovp_defines.h
@@ -39,16 +41,18 @@ namespace OpenViBEPlugins
 
 		protected:
 
-			::FILE* m_pFile;
+			std::ofstream m_oFileStream;
+
 			OpenViBE::CString m_sSeparator;
 			OpenViBE::CIdentifier m_oTypeIdentifier;
-			OpenViBE::boolean m_bUseCompression;
 			OpenViBE::boolean m_bFirstBuffer;
 			OpenViBE::boolean (OpenViBEPlugins::FileIO::CBoxAlgorithmCSVFileWriter::*m_fpRealProcess)(void);
 
 			OpenViBE::Kernel::IAlgorithmProxy* m_pStreamDecoder;
 			OpenViBE::IMatrix* m_pMatrix;
 			OpenViBE::boolean m_bDeleteMatrix;
+
+			OpenViBE::uint64 m_ui64SampleCount;
 
 			OpenViBE::Kernel::TParameterHandler < const OpenViBE::IMemoryBuffer* > ip_pMemoryBuffer;
 			OpenViBE::Kernel::TParameterHandler < OpenViBE::IMatrix* > op_pMatrix;
@@ -63,22 +67,11 @@ namespace OpenViBEPlugins
 
 			virtual OpenViBE::boolean onInputTypeChanged(OpenViBE::Kernel::IBox& rBox, const OpenViBE::uint32 ui32Index)
 			{
+				// @note this doesn't currently work as ConnectionEditor replaces the value set here by the gtk widget text field contents				
 				OpenViBE::CIdentifier l_oTypeIdentifier;
 				rBox.getInputType(ui32Index, l_oTypeIdentifier);
-				if(this->getTypeManager().isDerivedFromStream(l_oTypeIdentifier, OV_TypeId_StreamedMatrix))
-				{
-					rBox.setInputName(ui32Index, "Streamed matrix");
-				}
-				else if(l_oTypeIdentifier==OV_TypeId_Stimulations)
-				{
-					rBox.setInputName(ui32Index, "Stimulations");
-				}
-				else
-				{
-					rBox.setInputName(ui32Index, "Streamed matrix");
-					rBox.setInputType(ui32Index, OV_TypeId_Signal);
-					return false;
-				}
+				rBox.setInputName(ui32Index, this->getTypeManager().getTypeName(l_oTypeIdentifier));
+
 				return true;
 			}
 
@@ -111,12 +104,15 @@ namespace OpenViBEPlugins
 				rBoxAlgorithmPrototype.addInput  ("Input signal",  OV_TypeId_Signal);
 				rBoxAlgorithmPrototype.addSetting("Filename",         OV_TypeId_Filename, "record-[$core{date}-$core{time}].csv");
 				rBoxAlgorithmPrototype.addSetting("Column separator", OV_TypeId_String, ";");
-				rBoxAlgorithmPrototype.addSetting("Use compression",  OV_TypeId_Boolean, "false");
+				rBoxAlgorithmPrototype.addSetting("Precision",        OV_TypeId_Integer, "10");
 				rBoxAlgorithmPrototype.addFlag(OpenViBE::Kernel::BoxFlag_CanModifyInput);
 
 				rBoxAlgorithmPrototype.addInputSupport(OV_TypeId_Signal);
+				rBoxAlgorithmPrototype.addInputSupport(OV_TypeId_StreamedMatrix);
 				rBoxAlgorithmPrototype.addInputSupport(OV_TypeId_Spectrum);
 				rBoxAlgorithmPrototype.addInputSupport(OV_TypeId_Stimulations);
+				rBoxAlgorithmPrototype.addInputSupport(OV_TypeId_FeatureVector);
+
 				return true;
 			}
 

commit 6cafbf09f6de9f16bd845ebadadbe501108e4afc

Plugins: CSV Writer was not behaving correctly with epoched signal, fixed
Bug was introduced in 0de81b15d7f7b1ddd0cc68c238dcb2e06eca74b9 [formerly 92f65e5c617913fff10f5b2e6a7cf03113c037a4]


Former-commit-id: e89af94eff5c6582b8425e09715c38f9e952b3df

plugins/processing/file-io/src/box-algorithms/csv/ovpCBoxAlgorithmCSVFileWriter.cpp
@@ -208,9 +208,11 @@ boolean CBoxAlgorithmCSVFileWriter::process_streamedMatrix(void)
 				}
 				else if(m_oTypeIdentifier==OV_TypeId_Signal)
 				{
-					uint64 op_ui64SamplingFrequency =  ((OpenViBEToolkit::TSignalDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputSamplingRate();
+					const uint64 l_ui64SamplingFrequency =  ((OpenViBEToolkit::TSignalDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputSamplingRate();
+					const uint64 l_ui64TimeOfNthSample = ITimeArithmetics::sampleCountToTime(l_ui64SamplingFrequency, s); // assuming chunk start is 0
+					const uint64 l_ui64SampleTime = l_ui64StartTime+l_ui64TimeOfNthSample;
 
-					m_oFileStream << ITimeArithmetics::timeToSeconds(ITimeArithmetics::sampleCountToTime(static_cast<uint64>(op_ui64SamplingFrequency), m_ui64SampleCount+s));
+					m_oFileStream << ITimeArithmetics::timeToSeconds(l_ui64SampleTime);
 				}
 				else if(m_oTypeIdentifier==OV_TypeId_Spectrum) 
 				{
@@ -225,18 +227,18 @@ boolean CBoxAlgorithmCSVFileWriter::process_streamedMatrix(void)
 				{
 					if(m_oTypeIdentifier==OV_TypeId_Signal)
 					{
-						uint64 op_ui64SamplingFrequency =  ((OpenViBEToolkit::TSignalDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputSamplingRate();
+						const uint64 l_ui64SamplingFrequency =  ((OpenViBEToolkit::TSignalDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputSamplingRate();
 
-						m_oFileStream << m_sSeparator.toASCIIString() << (uint64)op_ui64SamplingFrequency;
+						m_oFileStream << m_sSeparator.toASCIIString() << (uint64)l_ui64SamplingFrequency;
 
 						m_bFirstBuffer=false;
 					}
 					else if(m_oTypeIdentifier==OV_TypeId_Spectrum)
 					{
-						IMatrix* op_pMinMaxFrequencyBand =  ((OpenViBEToolkit::TSpectrumDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputMinMaxFrequencyBands();
+						const IMatrix* l_pMinMaxFrequencyBand =  ((OpenViBEToolkit::TSpectrumDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputMinMaxFrequencyBands();
 
-						m_oFileStream << m_sSeparator.toASCIIString() << op_pMinMaxFrequencyBand->getBuffer()[s*2+0];
-						m_oFileStream << m_sSeparator.toASCIIString() << op_pMinMaxFrequencyBand->getBuffer()[s*2+1];
+						m_oFileStream << m_sSeparator.toASCIIString() << l_pMinMaxFrequencyBand->getBuffer()[s*2+0];
+						m_oFileStream << m_sSeparator.toASCIIString() << l_pMinMaxFrequencyBand->getBuffer()[s*2+1];
 					}
 					else
 					{
@@ -285,7 +287,7 @@ boolean CBoxAlgorithmCSVFileWriter::process_stimulation(void)
 		}
 		if(m_pStreamDecoder->isBufferReceived())
 		{
-			IStimulationSet* l_pStimulationSet = ((OpenViBEToolkit::TStimulationDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputStimulationSet();
+			const IStimulationSet* l_pStimulationSet = ((OpenViBEToolkit::TStimulationDecoder < CBoxAlgorithmCSVFileWriter >*)m_pStreamDecoder)->getOutputStimulationSet();
 			for(uint32 j=0; j<l_pStimulationSet->getStimulationCount(); j++)
 			{
 				m_oFileStream << ITimeArithmetics::timeToSeconds(l_pStimulationSet->getStimulationDate(j))