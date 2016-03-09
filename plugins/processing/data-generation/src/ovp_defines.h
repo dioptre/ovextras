9e1064b5b02901e98fe3859d08f0ffbecc852fa3
Jussi Lindgren <openvibe-dev@inria.fr>
2016-03-09 13:59:59 +0100

    CertiViBE: Fixed compilation of data generation plugin
    Former-commit-id: eff1b7081bdb43674336ef0fdbe3893e0ca35d4e

--- plugins/processing/data-generation/src/ovp_defines.h
+++ plugins/processing/data-generation/src/ovp_defines.h
@@ -7,13 +7,7 @@
 //___________________________________________________________________//
 //                                                                   //
 
-
-
-
-#define OVP_ClassId_SinusSignalGeneratorDesc                OpenViBE::CIdentifier(0x2633AFA2, 0x6974E32F)
 #define OVP_ClassId_TimeSignalGeneratorDesc                 OpenViBE::CIdentifier(0x57AD8655, 0x1966B4DC)
-#define OVP_ClassId_NoiseGeneratorDesc			            OpenViBE::CIdentifier(0x7237458A, 0x1F312C4A)
-#define OVP_ClassId_NoiseGenerator			                OpenViBE::CIdentifier(0x0E3929F1, 0x15AF76B9)
 
 //___________________________________________________________________//
 //                                                                   //
@@ -21,8 +15,6 @@
 //___________________________________________________________________//
 //                                                                   //
 
-
-#define OVP_ClassId_SinusSignalGenerator                    OpenViBE::CIdentifier(0x7E33BDB8, 0x68194A4A)
 #define OVP_ClassId_TimeSignalGenerator                     OpenViBE::CIdentifier(0x28A5E7FF, 0x530095DE)
 
 
@@ -35,7 +27,6 @@
 #ifdef TARGET_HAS_ThirdPartyOpenViBEPluginsGlobalDefines
  #include "ovp_global_defines.h"
 #endif // TARGET_HAS_ThirdPartyOpenViBEPluginsGlobalDefines
-
 
 
 #endif // __OpenViBEPlugins_Defines_H__
