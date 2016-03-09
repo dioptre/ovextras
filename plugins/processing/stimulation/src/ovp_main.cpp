c8421517e86de110fd797225a9034d643ec2b1ae
Jussi Lindgren <openvibe-dev@inria.fr>
2016-03-09 14:37:37 +0100

    CertiViBE: Removed boxes from the stimulation plugin
    Former-commit-id: 42f922767e61317b6f095f9ff8dd45d0f8733406

--- plugins/processing/stimulation/src/ovp_main.cpp
+++ plugins/processing/stimulation/src/ovp_main.cpp
@@ -2,33 +2,16 @@
 #include "ovp_defines.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmClockStimulator.h"
-#include "box-algorithms/ovpCKeyboardStimulator.h"
-#include "box-algorithms/ovpCSignChangeDetector.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmPlayerController.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmStimulationMultiplexer.h"
 #include "box-algorithms/ovpCBoxAlgorithmStimulationVoter.h"
 
-#include "box-algorithms/ovpCBoxAlgorithmRunCommand.h"
-#if defined TARGET_HAS_ThirdPartyLua
- #include "box-algorithms/ovpCBoxAlgorithmLuaStimulator.h"
-#endif // TARGET_HAS_ThirdPartyLua
-#if defined TARGET_HAS_ThirdPartyOpenAL
- #include "box-algorithms/ovpCBoxAlgorithmOpenALSoundPlayer.h"
-#endif // TARGET_HAS_ThirdPartyOpenAL
-
-#include "box-algorithms/adaptation/ovpCBoxAlgorithmStimulationFilter.h"
-
-#include "box-algorithms/ovpCBoxAlgorithmP300SpellerStimulator.h"
-#include "box-algorithms/ovpCBoxAlgorithmP300IdentifierStimulator.h"
 
 #include "box-algorithms/ovpCBoxAlgorithmTimeout.h"
 
 OVP_Declare_Begin();
-	rPluginModuleContext.getTypeManager().registerEnumerationType(OVP_TypeId_StimulationFilterAction, "Stimulation Filter Action");
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_StimulationFilterAction, "Select", OVP_TypeId_StimulationFilterAction_Select.toUInteger());
-	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OVP_TypeId_StimulationFilterAction, "Reject", OVP_TypeId_StimulationFilterAction_Reject.toUInteger());
 
 	rPluginModuleContext.getTypeManager().registerEnumerationType(OV_TypeId_PlayerAction, "Player Action");
 	rPluginModuleContext.getTypeManager().registerEnumerationEntry(OV_TypeId_PlayerAction, "Play", OV_TypeId_PlayerAction_Play.toUInteger());
@@ -38,28 +21,9 @@ OVP_Declare_Begin();
 
 	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmClockStimulatorDesc);
 
-#if defined(TARGET_HAS_ThirdPartyGTK)
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CKeyboardStimulatorDesc);
-#endif
-
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CSignChangeDetectorDesc);
 
 	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmPlayerControllerDesc);
 	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmStimulationMultiplexerDesc);
-
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmRunCommandDesc);
-#if defined TARGET_HAS_ThirdPartyLua
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmLuaStimulatorDesc);
-#endif // TARGET_HAS_ThirdPartyLua
-
-#if defined TARGET_HAS_ThirdPartyOpenAL
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmOpenALSoundPlayerDesc);
-#endif // TARGET_HAS_ThirdPartyOpenAL
-
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmStimulationFilterDesc);
-
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmP300SpellerStimulatorDesc);
-	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmP300IdentifierStimulatorDesc);
 
 	OVP_Declare_New(OpenViBEPlugins::Stimulation::CBoxAlgorithmTimeoutDesc);
 
