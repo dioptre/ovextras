c9eab02e734dc77e195667b7a93631dd71159c3b
Thierry Gaugry <thierry.gaugry@inria.fr>
2017-06-01 11:35:39 +0200

    update build script with --install-dir and --build-dir options
    Former-commit-id: d6332916ce3fd74e616c398a062a29fa660ccc55

--- scripts/win32-build.cmd
+++ scripts/win32-build.cmd
@@ -6,6 +6,9 @@ REM no options / set to default
 set BuildType=Release
 set InitEnvScript=win32-init_env_command.cmd
 set PAUSE=pause
+
+echo  (%*)
+:parameter_parse
 if /i "%1"=="-h" (
 	echo Usage: win32-build.cmd [Build Type] [Init-env Script]
 	echo -- Build Type option can be : --release (-r^), --debug (-d^) or --debug-symbols. Default is Release.
@@ -20,35 +23,59 @@ if /i "%1"=="-h" (
 	exit 0
 ) else if /i "%1"=="--no-pause" (
 	set PAUSE=echo ""
+	SHIFT
+	Goto parameter_parse
 ) else if /i "%1"=="-d" (
 	set BuildType=Debug
-	if not "%2"=="" (
-		set !InitEnvScript!=%2
-	)
+	REM set !InitEnvScript!=%2
+	SHIFT
+	Goto parameter_parse
 ) else if /i "%1"=="--debug" (
 	set BuildType=Debug
-	if not "%2"=="" (
-		set !InitEnvScript!=%2
-	)	
+	REM set !InitEnvScript!=%2
+	SHIFT
+	Goto parameter_parse
 ) else if /i "%1"=="--debug-symbols" (
 	set BuildType=RelWithDebInfo
-	if not "%2"=="" (
-		set !InitEnvScript!=%2
-	)		
+	REM set !InitEnvScript!=%2
+	SHIFT
+	Goto parameter_parse
 ) else if /i "%1"=="-r" (
 	set BuildType=Release
-	if not "%2"=="" (
-		set !InitEnvScript!=%2
-	)	
+	REM set !InitEnvScript!=%2
+	SHIFT
+	Goto parameter_parse
 ) else if /i "%1"=="--release" (
 	set BuildType=Release
-	if not "%2"=="" (
-		set !InitEnvScript!=%2
-	)
-) else if not "%1"=="" (
-	set BuildType=Release
-	set !InitEnvScript!=%1
+	REM set !InitEnvScript!=%2
+	SHIFT
+	Goto parameter_parse
+) else if /i "%1"=="--studiosdk" (
+	set studiosdk="-DSTUDIO_SDK_PATH=%2"
+	SHIFT
+	SHIFT
+	Goto parameter_parse
+) else if /i "%1"=="--build-dir" (
+	set ov_build_dir=%2
+	SHIFT
+	SHIFT
+	Goto parameter_parse
+) else if /i "%1"=="--install-dir" (
+	set ov_install_dir=%2
+	SHIFT
+	SHIFT
+	Goto parameter_parse
+) else if not "%1" == "" (
+	echo unrecognized option [%1]
+	Goto terminate_error
 )
+
+REM else if not "%1"=="" (
+	REM set BuildType=Release
+	REM set !InitEnvScript!=%1
+	REM SHIFT
+	REM Goto parameter_parse
+REM )
 
 if /i "!InitEnvScript!"=="win32-init_env_command.cmd" (
 	echo No script specified. Default will be used.
@@ -57,6 +84,8 @@ if /i "!InitEnvScript!"=="win32-init_env_command.cmd" (
 echo --
 echo build type is set to: %BuildType%.
 echo Init-env Script to be called: !InitEnvScript!.
+echo bd %ov_build_dir%
+echo sdk %studiosdk%
 echo --
 
 REM #######################################################################################
@@ -66,9 +95,13 @@ call "!InitEnvScript!"
 REM #######################################################################################
 
 set ov_script_dir=%CD%
-set ov_build_dir=%ov_script_dir%\..\local-tmp\nmake-%BuildType%
-set ov_install_dir=%ov_script_dir%\..\dist
 
+if not defined ov_build_dir (
+	set ov_build_dir=%ov_script_dir%\..\local-tmp\nmake-%BuildType%
+)
+if not defined ov_install_dir (
+	set ov_install_dir=%ov_script_dir%\..\dist
+)
 echo.
 echo _______________________________________________________________________________
 echo.
@@ -79,7 +112,7 @@ cd /D %ov_build_dir%
 echo Generating makefiles for %VSCMake% using %BuildType% config.
 echo Building to %ov_build_dir% ...
 
-%ov_script_dir%\..\dependencies\cmake\bin\cmake %ov_script_dir%\.. -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=%BuildType% -DCMAKE_INSTALL_PREFIX=%ov_install_dir%
+%ov_script_dir%\..\dependencies\cmake\bin\cmake %ov_script_dir%\.. -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=%BuildType% -DCMAKE_INSTALL_PREFIX=%ov_install_dir% %studiosdk%
 IF NOT "!ERRORLEVEL!" == "0" goto terminate_error
 
 echo.
