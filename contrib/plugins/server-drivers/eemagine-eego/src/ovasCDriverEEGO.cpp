0e9f79352391567eac9e198fe10c0f4ec4868a02
cedric.riou <cedric.riou@inria.fr>
2018-03-28 11:18:18 +0200

    EEGO: redefine std::make_unique (C++14) for c++11 compliance
    Former-commit-id: a22dcbca3d8cf8d8d00e7b39be0f33ab0bc05ebf

--- contrib/plugins/server-drivers/eemagine-eego/src/ovasCDriverEEGO.cpp
+++ contrib/plugins/server-drivers/eemagine-eego/src/ovasCDriverEEGO.cpp
@@ -6,6 +6,12 @@
 #include <exception>
 #include <memory>
 
+#if defined TARGET_OS_Linux || defined TARGET_OS_MacOS
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+#endif
+
 // OV includes
 #include <toolkit/ovtk_all.h>
 #include <openvibe/ovITimeArithmetics.h>
@@ -28,6 +34,39 @@ using namespace OpenViBEAcquisitionServer;
 using namespace OpenViBE;
 using namespace Kernel;
 namespace es = eemagine::sdk;
+
+#if defined TARGET_OS_Linux || defined TARGET_OS_MacOS
+namespace std {
+    template<class T> struct _Unique_if {
+        typedef unique_ptr<T> _Single_object;
+    };
+
+    template<class T> struct _Unique_if<T[]> {
+        typedef unique_ptr<T[]> _Unknown_bound;
+    };
+
+    template<class T, size_t N> struct _Unique_if<T[N]> {
+        typedef void _Known_bound;
+    };
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Single_object
+        make_unique(Args&&... args) {
+            return unique_ptr<T>(new T(std::forward<Args>(args)...));
+        }
+
+    template<class T>
+        typename _Unique_if<T>::_Unknown_bound
+        make_unique(size_t n) {
+            typedef typename remove_extent<T>::type U;
+            return unique_ptr<T>(new U[n]());
+        }
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Known_bound
+        make_unique(Args&&...) = delete;
+}
+#endif
 
 //___________________________________________________________________//
 //                                                                   //
@@ -147,8 +186,8 @@ boolean CDriverEEGO::check_configuration(void)
 			GTK_DIALOG_MODAL, // Behavoir
 			GTK_MESSAGE_QUESTION, // Type
 			GTK_BUTTONS_OK_CANCEL, // buttons
-			"The channels masks are set to only stream %d channels, but %d channels should be streamed.\n"
-			"Change the amount of channels to %d?", l_iAllChannels, m_oHeader.getChannelCount(), l_iAllChannels);
+			"The channels masks are set to only stream %ld channels, but %d channels should be streamed.\n"
+			"Change the amount of channels to %ld?", l_iAllChannels, m_oHeader.getChannelCount(), l_iAllChannels);
 		gint l_iResult = gtk_dialog_run(GTK_DIALOG(l_pDialogue));
 		gtk_widget_destroy(l_pDialogue);
 		l_pDialogue = nullptr;
